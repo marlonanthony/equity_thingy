4b22f150ea4121ce1921a2992c888ebd29da2ef3
marlonanthony <mad1083@gmail.com>
2019-05-19 20:25:04 -0400

    refactored and styled

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,13 +4,13 @@ import './App.css'
 
 // goofy ass API with spaces in its keys work around
 const fromCurrencyCode = '1. From_Currency Code',
-      fromCurrencyName = '2. From_Currency Name',
-      toCurrencyCode = '3. To_Currency Code',
-      toCurrencyName = '4. To_Currency Name', 
+      // fromCurrencyName = '2. From_Currency Name',
+      // toCurrencyCode = '3. To_Currency Code',
+      // toCurrencyName = '4. To_Currency Name', 
       exchangeRate = '5. Exchange Rate',
-      lastRefreshed = '6. Last Refreshed',
-      timeZone = '7. Time Zone',
-      bidPrice = '8. Bid Price',
+      // lastRefreshed = '6. Last Refreshed',
+      // timeZone = '7. Time Zone',
+      // bidPrice = '8. Bid Price',
       askPrice = '9. Ask Price'
 
 const App = () => {
@@ -19,7 +19,11 @@ const App = () => {
   const [equity, setEquity] = useState({}) 
   const [isLoading, setIsLoading] = useState(false) 
   const [bankRoll, setBankRoll] = useState(100000)
-  let profitLoss = 0
+  const [pipDif, setPipDif] = useState(0)
+
+  let exchangeRatePurchasedAt = JSON.parse(localStorage.getItem('exchangeRate')) || '😿',
+      purchasedBankRoll = JSON.parse(localStorage.getItem('bankRoll')),
+      pL = pipDif * 100000
 
   async function fetchData () {
     try {
@@ -35,28 +39,33 @@ const App = () => {
 
   useEffect(() => {
     fetchData() 
-  }, [profitLoss, currency])
+  }, [currency]) // Left toCurrency out of deps because I dont want to make http req onChange
 
   return (
     // <main className='container'>
       <div className="App">
         <section >
-          <h1>Bank Roll {bankRoll +' '+ currency}</h1>
+          <h1>Bank Roll <span>{(purchasedBankRoll + pL || bankRoll) +' EUR'}</span></h1>
           <div className='buy_sell'>
             <button onClick={(e) => {
-              const newBankRoll = (bankRoll * equity[exchangeRate]).toFixed(4) 
+              // const newBankRoll = (bankRoll * equity[exchangeRate]).toFixed(4) 
+              const newBankRoll = (bankRoll).toFixed(4) 
+              const storedExchangeRate = (equity[exchangeRate]).toFixed(4) 
               setBankRoll(newBankRoll)
-              localStorage.setItem('exchangeRate', equity[exchangeRate])
-              setCurrency(currency === 'USD' ? 'EUR' : 'USD') 
-              setToCurrency(toCurrency === 'USD' ? 'EUR' : 'USD') 
-            }}>Buy</button>
-            <p>Profit/Loss: <span>{profitLoss +' '+ currency}</span></p>
+              localStorage.setItem('exchangeRate', storedExchangeRate)
+              localStorage.setItem('bankRoll', (bankRoll).toFixed(4))
+              console.log()
+              // localStorage.setItem('bankRoll', (bankRoll * equity[exchangeRate]).toFixed(4))
+              }}>Buy
+            </button>
+            <p>PipDif: <span>{pipDif}</span></p>
+            <p>Profit/Loss: <span>{pL +' '+ currency}</span></p>
+            <p>PurchasedAt: <span>{exchangeRatePurchasedAt}</span></p>
             <button onClick={() => {
-              const storedExchangeRate = JSON.parse(localStorage.getItem('exchangeRate'))
-              profitLoss = equity[exchangeRate] - storedExchangeRate
-              setCurrency(currency === 'USD' ? 'EUR' : 'USD') 
-              setToCurrency(toCurrency === 'USD' ? 'EUR' : 'USD') 
-            }}> Sell </button>
+              const pipDifference = (equity[exchangeRate] - exchangeRatePurchasedAt).toFixed(4)
+              setPipDif(pipDifference)
+              }}>Sell 
+            </button>
           </div>
         </section>
         <section className='exchange_details'>
