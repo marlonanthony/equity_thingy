ed4086ec43f65986453c1446fcd947e8bf96186a
marlonanthony <mad1083@gmail.com>
2019-05-29 04:37:00 -0400

    added auth logic to buy/sell buttons

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,44 +1,66 @@
-import React, { useState, useEffect, useCallback } from 'react'
-import { Query } from 'react-apollo'
+import React, { useState, useEffect } from 'react'
+import { Query, Mutation } from 'react-apollo'
 import gql from 'graphql-tag'
 import axios from 'axios' 
-import jwt from 'jsonwebtoken'
+// import jwt from 'jsonwebtoken'
 
 import keys from './keys_dev'
-// import Login from './components/Login' 
-import Logout from './components/Logout'
 import Header from './components/Header'
 import './App.css'
 
-const GET_USERS = gql`
-  query GetUsers {
-    isLoggedIn @client
-    token @client
-    users {
-      id
-      currencyPairs {
+const IS_LOGGED_IN = gql`
+    query IsUserLogginIn {
+        isLoggedIn @client
+        token @client
+    }
+`
+
+const BUY_PAIR = gql`
+  mutation BuyPair($pair: String!, $lotSize: Int!, $purchasedAt: Float!) {
+    buyPair(pair: $pair, lotSize: $lotSize, purchasedAt: $purchasedAt){
+      success
+      message
+      currencyPair {
         id
-        pair 
+        pair
         lotSize
-        pipDif
-        profitLoss
         purchasedAt
-        soldAt
         open
+        createdAt
       }
     }
   }
 `
 
-const GET_USER = gql`
-  query GetUser($userId: ID!) {
-    user(id: $userId) {
-      email
-      id
+// const GET_USERS = gql`
+//   query GetUsers {
+//     isLoggedIn @client
+//     token @client
+//     users {
+//       id
+//       currencyPairs {
+//         id
+//         pair 
+//         lotSize
+//         pipDif
+//         profitLoss
+//         purchasedAt
+//         soldAt
+//         open
+//       }
+//     }
+//   }
+// `
 
-    }
-  }
-`
+// const GET_USER = gql`
+//   query GetUser($userId: ID!) {
+//     user(id: $userId) {
+//       email
+//       id
+
+//     }
+//   }
+// `
 
 // goofy ass API with spaces in its keys work around
 const exchangeRate = '5. Exchange Rate',
@@ -90,79 +112,87 @@ const App = () => {
               </div>
             )}
           </div>
-        ) */}
-
-
-
-          <main className='container'>
-            <div className="App">
-              <section >
-                <h1>Bank Roll <span>{(purchasedBankRoll + pL || bankRoll) +' EUR'}</span></h1>
-                <div className='buy_sell'>
-                  <button onClick={(e) => {
-                    const newBankRoll = (bankRoll).toFixed(4) 
-                    const storedExchangeRate = (equity[exchangeRate])//.toFixed(4) 
-                    setBankRoll(newBankRoll)
-                    localStorage.setItem('exchangeRate', storedExchangeRate)
-                    localStorage.setItem('bankRoll', (bankRoll).toFixed(4))
-                    }}>Buy
-                  </button>
-                  <p>PipDif: <span>{pipDif}</span></p>
-                  <p>Profit/Loss: <span>{(pL +' '+ currency)}</span></p>
-                  <p>PurchasedAt: <span>{exchangeRatePurchasedAt}</span></p>
-                  <button onClick={() => {
-                    const pipDifference = (equity[exchangeRate] - exchangeRatePurchasedAt).toFixed(4)
-                    setPipDif(pipDifference)
-                    }}>Sell 
-                  </button>
-                </div>
-              </section>
-              <section className='exchange_details'>
-                <h1>Currency Exchange</h1>
-                <form onSubmit={(e) => {
-                  fetchData() 
-                  setPipDif((equity[exchangeRate] - exchangeRatePurchasedAt).toFixed(4))
-                  e.preventDefault() 
-                }}> 
-                  <input 
-                    placeholder={currency} 
-                    name='currency' 
-                    value={currency} 
-                    // onChange={e => setCurrency(e.target.value)}
-                    readOnly
-                  />
-                  <input 
-                    placeholder='To' 
-                    // onChange={e => setToCurrency(e.target.value)}
-                    name='toCurrency' 
-                    value={toCurrency} 
-                    readOnly
-                  />
-                  <button type='submit'>Update</button>
-                </form>
-                { isLoading ? <h1>LOADING</h1> : (
-                  equity && Object.keys(equity).map(val => (
-                    val.includes('Last Refreshed') 
-                    ? (
-                        <div key={val[0]} className='data'>
-                          <p className='key'>{`${val.slice(2)}:`}</p> 
-                          <p>{`${new Date(equity[val]).toLocaleString()}`}</p>
-                        </div>
-                      ) 
-                    : (
-                        <div key={val[0]} className='data'>
-                          <p className='key'>{`${val.slice(2).replace(/_/g, ' ')}:`}</p> 
-                          <p>{`${equity[val]}`}</p>
-                        </div>
-                      )
-                  )) 
-                )}
-              </section>
-            </div>
-          </main>
         )
-      }}
-    {/* </Query> */}
+    */}
+    <Query query={IS_LOGGED_IN}>
+      {({ data }) => (
+      <main className='container'>
+        <div className="App">
+          {data && data.isLoggedIn && <section>
+            <h1>Bank Roll <span>{(purchasedBankRoll + pL || bankRoll) +' EUR'}</span></h1>
+            <div className='buy_sell'>
+              <button onClick={(e) => {
+                const newBankRoll = (bankRoll).toFixed(4) 
+                const storedExchangeRate = (equity[exchangeRate])//.toFixed(4) 
+                setBankRoll(newBankRoll)
+                localStorage.setItem('exchangeRate', storedExchangeRate)
+                localStorage.setItem('bankRoll', (bankRoll).toFixed(4))
+              }}>Buy
+              </button>
+              <p>PipDif: <span>{pipDif}</span></p>
+              <p>Profit/Loss: <span>{(pL +' '+ currency)}</span></p>
+              <p>PurchasedAt: <span>{exchangeRatePurchasedAt}</span></p>
+              <button onClick={() => {
+                const pipDifference = (equity[exchangeRate] - exchangeRatePurchasedAt).toFixed(4)
+                setPipDif(pipDifference)
+                }}>Sell 
+              </button>
+              
+            </div>
+          </section>}
+          <section className='exchange_details'>
+            <h1>Currency Exchange</h1>
+            <form onSubmit={(e) => {
+              fetchData() 
+              setPipDif((equity[exchangeRate] - exchangeRatePurchasedAt).toFixed(4))
+              e.preventDefault() 
+            }}> 
+              <input 
+                placeholder={currency} 
+                name='currency' 
+                value={currency} 
+                // onChange={e => setCurrency(e.target.value)}
+                readOnly
+              />
+              <input 
+                placeholder='To' 
+                // onChange={e => setToCurrency(e.target.value)}
+                name='toCurrency' 
+                value={toCurrency} 
+                readOnly
+              />
+              <select 
+                onChange={e => setToCurrency(e.target.value)}
+                name='toCurrency'
+                value={toCurrency}
+              >
+                <option>USD</option>
+                <option>GBP</option>
+              </select>
+              <button type='submit'>Update</button>
+            </form>
+            { isLoading ? <h1>LOADING</h1> : (
+              equity && Object.keys(equity).map(val => (
+                val.includes('Last Refreshed') 
+                ? (
+                    <div key={val[0]} className='data'>
+                      <p className='key'>{`${val.slice(2)}:`}</p> 
+                      <p>{`${new Date(equity[val]).toLocaleString()}`}</p>
+                    </div>
+                  ) 
+                : (
+                    <div key={val[0]} className='data'>
+                      <p className='key'>{`${val.slice(2).replace(/_/g, ' ')}:`}</p> 
+                      <p>{`${equity[val]}`}</p>
+                    </div>
+                  )
+              )) 
+            )}
+          </section>
+        </div>
+      </main>
+      )}
+    </Query>
     </React.Fragment>
   )
 }
