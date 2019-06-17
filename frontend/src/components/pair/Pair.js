cb9b57a7a79f4d1730c4abc5418cddebdb028c2a
marlonanthony <mad1083@gmail.com>
2019-06-17 13:09:40 -0400

    styled pair.js

--- frontend/src/components/pair/Pair.js
+++ frontend/src/components/pair/Pair.js
@@ -2,6 +2,8 @@ import React from 'react'
 import gql from 'graphql-tag' 
 import { Query, Mutation } from 'react-apollo'
 
+import './Pair.css'
+
 const CURRENCY_PAIR_INFO = gql`
   query CurrencyPairInfo($fc: String!, $tc: String!) {
     isLoggedIn @client
@@ -40,7 +42,8 @@ const SELL_PAIR = gql`
 export default function Pair(props) {
     const { lotSize, pair, purchasedAt, id } = props.location.state.pair,
           currency = pair.split('/')[0],
-          toCurrency = pair.split('/')[1]
+          toCurrency = pair.split('/')[1],
+          { bankroll } = props.location.state 
           
     return (
         <Query query={CURRENCY_PAIR_INFO} variables={{ fc: currency, tc: toCurrency }}>
@@ -51,32 +54,38 @@ export default function Pair(props) {
                       pipDif = (bidPrice - purchasedAt).toFixed(4) 
                 
                 return data && (
-                    <div style={{ textAlign: 'center' }}>
-                        <p>Lot size: { lotSize.toLocaleString() + '.00' }</p>
-                        <p>Currency pair: { pair }</p>
-                        <p>Purchased price: { purchasedAt }</p>
-                        <br />
-                        <p>Last Refreshed: { new Date(lastRefreshed).toLocaleTimeString() }</p>
-                        <p>Current bid price: { Number(bidPrice).toFixed(4) }</p>
-                        <p>Current pip difference: { pipDif }</p>
-                        <p>Potential profilt/loss: { Math.round(pipDif * lotSize) + '.00' }</p>
-                        <button onClick={() => refetch()}>Refetch</button>
-                        <Mutation
-                            mutation={SELL_PAIR}
-                            variables={{ id, soldAt: +bidPrice }}>
-                            {(sellPair, { data, loading, error }) => {
-                                if(loading) return <h1>Loading...</h1>
-                                if(error) console.log(error) 
-                                return (
-                                    <div>
-                                        <button onClick={sellPair}>Sell</button>
-                                        <p>{ data && data.sellPair.success && 'Success: True' }</p>
-                                        <p>{ data && data.sellPair.message && data.sellPair.message }</p>
-                                        { data && data.sellPair.currencyPair && <p>Sold At: { data.sellPair.currencyPair.soldAt }</p> }
-                                    </div>
-                                )
-                            }}
-                        </Mutation>
+                    <div>
+                        <div className='pair_container'>
+                            <p><span>Bankroll: </span>{bankroll}</p>
+                            <Mutation
+                                mutation={SELL_PAIR}
+                                variables={{ id, soldAt: +bidPrice }}>
+                                {(sellPair, { data, loading, error }) => {
+                                    if(loading) return <h1>Loading...</h1>
+                                    if(error) return `Error ${error}` 
+                                    return (
+                                        <div className='pair_sell_button'>
+                                            <button onClick={sellPair}>Sell</button>
+                                            <p>{ data && data.sellPair.success && 'Success: True' }</p>
+                                            <p>{ data && data.sellPair.message && data.sellPair.message }</p>
+                                            { data && data.sellPair.currencyPair && <p>Sold At: { data.sellPair.currencyPair.soldAt }</p> }
+                                        </div>
+                                    )
+                                }}
+                            </Mutation>
+                        </div>
+                        <div className='pair'>
+                            <p><span>Lot size: </span>{ lotSize.toLocaleString() + '.00' }</p>
+                            <p><span>Currency pair: </span>{ pair }</p>
+                            <p><span>Purchased price: </span>{ purchasedAt }</p>
+                        </div>
+                        <div className='pair'>
+                            <button onClick={() => refetch()}>Refresh</button>
+                            <p><span>Last Refreshed: </span>{ new Date(lastRefreshed).toLocaleTimeString() }</p>
+                            <p><span>Current bid price: </span>{ Number(bidPrice).toFixed(4) }</p>
+                            <p><span>Current pip difference: </span>{ pipDif }</p>
+                            <p><span>Potential profilt/loss: </span>{ Math.round(pipDif * lotSize) + '.00' }</p>
+                        </div>
                     </div>
                 )
             }} 
